8fd726e3439c57d11ed26bf45c022824cacfd3f7
Yajat Vishwakk <yajat472001@gmail.com>
2019-08-19 21:42:45 +0530

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `The Wall`,
+    author: `Yajat Vishwakk`,
+    description: `I'm a ever-learning developer. I love Science and Technology and truly believe that it has the power to shape and create the future we will be living in. 
+    My thoughts are sporadic. This is the place 
+    I share my learning experience. 
+    Hope this helps.`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/vishwakkyajat`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/yajatvishwak`,
+      },
+    ],
+  },
+}
---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Change the content to add your own bio
+ */
+
+export default () => (
+  <Fragment>
+    Written by Yajat Vishwakk,<br></br>
+    with <span role="img">❤️</span>
+
+  </Fragment>
+)
